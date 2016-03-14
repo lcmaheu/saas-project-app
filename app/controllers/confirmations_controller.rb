f8c6aebfda18240e4e54119f0513cebfdb27bbf3
Mark Oveson <mark.oveson@gmail.com>
2016-03-14 02:13:28 -0600

    Bug fixes

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -62,6 +62,6 @@ class ConfirmationsController < Milia::ConfirmationsController
   private
 
   def set_confirmable()
-    @confirmable = User.find_or_initialize_with_error_by(:@confirmation_token, params[:@confirmation_token])
+    @confirmable = User.find_or_initialize_with_error_by(:confirmation_token, params[:confirmation_token])
   end
 end
