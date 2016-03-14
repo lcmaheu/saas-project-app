6bb10d16a50ddac102f1535cb34cd2b3020e02f6
Mark Oveson <mark.oveson@gmail.com>
2016-03-14 08:49:36 -0600

    Add projects scaffold and datepicker

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Rails.application.routes.draw do
 
+  resources :tenants do
+    resources :projects
+  end
   resources :members
   get 'home/index'
 
-   root :to => "home#index"
+  root :to => "home#index"
 
-    
+
   # *MUST* come *BEFORE* devise's definitions (below)
-  as :user do   
+  as :user do
     match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
 
-  devise_for :users, :controllers => { 
-    :registrations => "milia/registrations",
-    :confirmations => "confirmations",
-    :sessions => "milia/sessions", 
-    :passwords => "milia/passwords", 
+  devise_for :users, :controllers => {
+      :registrations => "milia/registrations",
+      :confirmations => "confirmations",
+      :sessions => "milia/sessions",
+      :passwords => "milia/passwords",
   }
 
 
